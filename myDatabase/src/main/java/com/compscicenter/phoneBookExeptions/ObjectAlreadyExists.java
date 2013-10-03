95ad90289c6079c2010158a8538185f31d9e4c4d
Gleb Rurenko <glips@boost-mac.(none)>
2013-10-03 23:53:51 +0400

    Удаляю ненужное

--- myDatabase/src/main/java/com/compscicenter/phoneBookExeptions/ObjectAlreadyExists.java
+++
@@ -1,7 +0,0 @@
-package phoneBookExeptions;
-
-public class ObjectAlreadyExists extends Exception {
-	public ObjectAlreadyExists(String msg) {
-		super(msg);
-	}
-}
--- myDatabase/src/main/java/com/compscicenter/phoneBookExeptions/ParsingError.java
+++
@@ -1,7 +0,0 @@
-package phoneBookExeptions;
-
-public class ParsingError extends Exception {
-	public ParsingError(String msg) {
-		super(msg);
-	}
-}
